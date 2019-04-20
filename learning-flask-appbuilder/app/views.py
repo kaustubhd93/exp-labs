2b5cae34f07e0ed871e69b1e4ceec533f7d33095
Kaustubh Desai <kaustubh.desai@rebelfoods.co>
2019-04-20 16:53:46 +0530

    Added test view file and an adhoc script.

--- learning-flask-appbuilder/app/views.py
+++ learning-flask-appbuilder/app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView
+from flask_appbuilder import ModelView, BaseView, expose
 from app import appbuilder, db
 
 """
@@ -20,6 +20,23 @@ from app import appbuilder, db
 """
     Application wide 404 error handler
 """
+class MyView(BaseView):
+    route_base = "/myview"
+
+    @expose('/method1/<string:paramOne>')
+    @has_access
+    def methodOne(self, paramOne):
+        res = "What up.... {}".format(paramOne)
+        return res
+
+    @expose('/method2/<string:paramOne>')
+    @has_access
+    def methodTwo(self, paramOne):
+        res = "This is from a different mentthod {} see? Get my point.".format(paramOne)
+        return res
+
+appbuilder.add_view_no_menu(MyView())
+
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
---
+++ python-scripts/generate_vocab_table.py
@@ -0,0 +1,7 @@
+import sys
+
+limit = int(sys.argv[1])
+print "| Count | Word | Synonym | Antonym | Sentence |"
+print "| ------| ------ | ------ | ------ | ------ |"
+for i in range(1,limit):
+    print "|{}| **** | | | |".format(i)
