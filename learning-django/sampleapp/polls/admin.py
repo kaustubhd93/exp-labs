d77db038b4697e968b382229fcca021af54d9ba9
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-03-20 21:25:54 +0530

    Added functionality to add questions from admin and also display related choices inline and alter or add the same.

--- learning-django/sampleapp/polls/admin.py
+++ learning-django/sampleapp/polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Question,Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoicesOfQuestion(admin.TabularInline):
+    model = Choice
+    # An empty field with option to add choice will be present.
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"]}),
+    ]
+    # Below is an InlineModelAdmin object which
+    inlines = [ChoicesOfQuestion]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
