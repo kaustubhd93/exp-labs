e923a209c412ccef697769e369e45bdd573e9fbc
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-03-20 11:00:12 +0530

    Added static files like basic styling css, some more enhanced changes in html files. Migrated to class based generic views to avoid redundancy. Noted all new learnings.

--- learning-django/sampleapp/polls/urls.py
+++ learning-django/sampleapp/polls/urls.py
@@ -9,10 +9,22 @@ from . import views
 # This is the application namespace.
 # This enables us to refer app specific templates in url blocks.
 app_name = "polls"
+# Below code is commented to show clear migration to Generic views and
+# tutorial progress.
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("another/", views.sample_app_view, name="sample_app_view"),
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     path("<int:question_id>/results", views.results, name="results"),
+#     path("<int:question_id>/votes", views.votes, name="votes"),
+# ]
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     path("another/", views.sample_app_view, name="sample_app_view"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results", views.results, name="results"),
+    # The DetailView generic view expects the primary key value captured from the URL 
+    # to be called "pk", so we’ve changed question_id to pk for the generic views.
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/votes", views.votes, name="votes"),
 ]
--- learning-django/sampleapp/polls/views.py
+++ learning-django/sampleapp/polls/views.py
@@ -1,29 +1,47 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    latest_questions = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_questions" : latest_questions}
-    return render(request, "polls/index.html", context)
+# Below code is commented to show clear migration to Generic views and
+# tutorial progress.
+# def index(request):
+#     latest_questions = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_questions" : latest_questions}
+#     return render(request, "polls/index.html", context)
 
-# Made this just for experimenting.
-def sample_app_view(request):
-    return HttpResponse("This view is from another path.")
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {"question" : question}
+#     return render(request, "polls/detail.html", context)
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {"question" : question}
-    return render(request, "polls/detail.html", context)
+class IndexView(generic.ListView):
+    # Variables "template_name", "context_object_name" and method "get_queryset"
+    # are a part of the ListView Syntax. By using these variables we are specifying which template
+    # to use and what should be the object name of the context.
+    # get_queryset will get the list of items to display.
+    template_name = "polls/index.html"
+    context_object_name = "latest_questions"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+# This view cannot be made generic.
 def votes(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -34,4 +52,9 @@ def votes(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
+        # Using reverse function in order to avoid hardcoding of redirecting url.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+# Made this just for experimenting.
+def sample_app_view(request):
+    return HttpResponse("This view is from another path.")
