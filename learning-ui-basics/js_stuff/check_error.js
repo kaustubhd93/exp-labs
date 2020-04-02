c6d974d278adaa29161af83e915efc3a3e5c4438
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-04-02 22:51:48 +0530

    Added notes and code for intro to javascript.

---
+++ learning-ui-basics/js_stuff/check_error.js
@@ -0,0 +1,4 @@
+"use strict";
+// alert("There will be an error!")
+alert("All fine now");
+[1,2].forEach(alert);
---
+++ learning-ui-basics/js_stuff/hello_js_alert.js
@@ -0,0 +1,4 @@
+"use strict";
+alert("I'm Javascript!");
+alert("hello");
+alert("I am the third alert here.");
