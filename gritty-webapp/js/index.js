6517453cc067968b76c1506be43db6b5767df1e1
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-09-18 17:02:56 +0530

    Removed IP addresses and added place holders.

--- gritty-webapp/js/index.js
+++ gritty-webapp/js/index.js
@@ -1,27 +1,25 @@
 let reqdomainDiv = document.getElementById('reqdomain');
 
 async function postData(url = '', data = {}) {
-    // Default options are marked with *
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
       headers: {
         //'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
     });
-    return response; // parses JSON response into native JavaScript objects
+    return response;
   }
   
-postData('http://192.168.1.3/api/', { "url": "https://www.hotstar.com/ads.txt" })
+postData('http://X.X.X.X/api/', { "data": "" })
 .then(res => (res.json()))
-.then(adstxt => {
-    console.log(adstxt)
-    reqdomainDiv.innerHTML += `<p> ${JSON.stringify(adstxt)} </p>`
+.then(resObj => {
+    console.log(resObj)
+    reqdomainDiv.innerHTML += `<p> ${JSON.stringify(resObj)} </p>`
 });
 
