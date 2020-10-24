0549f16b713c1073b19e17b5522aaf571114340d
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-10-24 21:12:51 +0530

    Added shell script to setup a k8s test env on centos

---
+++ shell-scripts/setup-minikube-n-deps-centos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Setting up minikube
+sudo yum install -y telnet vim git
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube version
+# Setting up kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.2/bin/linux/amd64/kubectl
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version
+# Setting up docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo docker run hello-world
+sudo usermod -aG docker centos
+echo "******************************************************************"
+echo "Logout and login again to check if you can run docker without sudo"
+echo "******************************************************************"
+
