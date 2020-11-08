8510fa159c4481f5102fa4803c68742abe7a84c6
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-11-08 20:51:20 +0530

    Added log messages and service level validation

--- shell-scripts/setup-minikube-n-deps-centos.sh
+++ shell-scripts/setup-minikube-n-deps-centos.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# Setting up minikube
+echo "Setting up minikube..."
 sudo yum install -y telnet vim git
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube version
-# Setting up kubectl
+echo "Setting up kubectl..."
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.19.2/bin/linux/amd64/kubectl
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 kubectl version
-# Setting up docker
+echo "Setting up docker..."
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
+docker_status=`sudo systemctl is-active docker`
+if [[ "$docker_status" != "active" ]]
+then
+    sudo systemctl start docker
+fi
 sudo docker run hello-world
-sudo usermod -aG docker centos
+sudo usermod -aG docker `whoami`
 echo "******************************************************************"
 echo "Logout and login again to check if you can run docker without sudo"
 echo "******************************************************************"
