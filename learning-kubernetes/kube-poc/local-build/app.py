6b8e374d8bb15b97b3543b7cfde2f1d8bf660ba4
kaustubhd93 <kaustubhdesai93@gmail.com>
2022-02-02 21:23:56 +0530

    Test configmap and secrets

--- learning-kubernetes/kube-poc/local-build/app.py
+++ learning-kubernetes/kube-poc/local-build/app.py
@@ -1,26 +1,14 @@
-import json
-import requests
-from flask import Flask, request
+import os
+import time 
 
-app = Flask(__name__)
-
-@app.route("/v1/data",methods = ["POST"])
-def get_data():
-    req_body = request.json
-    print(req_body)
-    if "cat" not in req_body["animal"]:
-        return json.dumps({"code" : 501, "msg" : "Animal not supported."})
-    msg = {"code" : 200, "msg" : ""} 
-    try:
-        response = requests.get("https://catfact.ninja/fact").text
-        msg["msg"] = json.loads(response)
-    except Exception as e:
-        print(str(e))
-        msg["code"], msg["msg"] = 500, "Something went wrong here..."
-        return json.dumps(msg)
-    return json.dumps(msg)
+while True:
+    print("*************************************************")
+    print(os.environ['CLIENT_ID'])
+    print(os.environ['SCOPE'])
+    print("*************************************************")
+    print(os.environ)
+    print("\n")
+    time.sleep(10)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081)
 
